31438d587252e478ed94197dd01b0e9c66263db7
JOHN <113880870+JOHN10102003@users.noreply.github.com>
2023-02-03 16:06:50 +0530

    Delete Troubling Raju.py

--- Troubling Raju.py
+++
@@ -1,9 +0,0 @@
-def reverse(s): 
-         str = "" 
-         for i in s: 
-                 str = i + str 
-         return str 
-  
- s=input("Enter the number: ") 
- print("Mirror image: ", end="") 
- print(reverse(s))
