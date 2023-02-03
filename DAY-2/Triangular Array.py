a5bf76d6bab5ebc4eb1162ad31d3af4a6cbd002b
JOHN <113880870+JOHN10102003@users.noreply.github.com>
2023-02-03 16:01:15 +0530

    Create Triangular Array.py

---
+++ DAY-2/Triangular Array.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the row limit: ")) 
+ for i in range(1,n+1): 
+     for j in range(0, n-i+1): 
+         print(' ', end='') 
+     c=1 
+     for j in range(1,i+1): 
+         print(' ', c, sep='', end='') 
+         c=c*(i-j)//j 
+     print()
