0cd0f432f0358d59e8856eb598fe0463e717e5e5
JOHN <113880870+JOHN10102003@users.noreply.github.com>
2023-02-04 14:34:31 +0530

    Update Valid Number.py

--- DAY-1/Valid Number.py
+++ DAY-1/Valid Number.py
@@ -1,18 +1,19 @@
-def isNumber(s): 
-     valid_patterns = ('xd','x.d','xd.','xd.d','xsd','xsd.', 
-               'xsd.d','xded','x.ded','xd.ed','xd.ded', 
-               'xsded','xsd.ed','xsd.ded', 'xdesd', 'x.desd',  
-               'xd.esd', 'xd.desd', 'xsdesd', 'xsd.esd',  
-               'xsd.desd', 'xs.d', 'xs.ded', 'xs.desd' 
-               ) 
-     patttern = 'x' 
-     for ch in s: 
-         if ch == '+' or ch == '-': patttern += 's' 
-         elif ch == 'e' or ch == 'E': patttern += 'e' 
-         elif ch.isdigit(): 
-             if patttern[-1] != 'd': patttern += 'd' 
-         elif ch == '.': patttern += '.' 
-         else: return False 
-     return patttern in valid_patterns 
- str1=input("String=") 
- print(isNumber(str1))
+def isnumber(s: str) -> bool:
+    num, exp, sign, dec=False, False, False, False
+    for c in s:
+        if c>='0' and c<='9': num=True
+        elif c=='e' or c=='E':
+            if exp or not num: return False
+            else: exp, num, sign, dec=True, False, False, False
+        elif c=='+' or c=='-':
+            if sign or num or dec: return False
+            else: sign=True
+        elif c=='.':
+            if dec or exp: return False
+            else: dec=True
+        else: return False
+    return num
+
+#main
+s=input("Enter a number: ")
+print(isnumber(s))
