c260069fd973ba1720ac009efcdfce5c6afcd484
JOHN <113880870+JOHN10102003@users.noreply.github.com>
2023-02-04 11:55:55 +0530

    Create Frequency.py

---
+++ DAY-1/Frequency.py
@@ -0,0 +1,17 @@
+def frequency(s):
+    freq=[0]*26
+    n=len(s)
+    for i in range(n):
+        freq[ord(s[i])-ord('a')]+=1
+    for i in range(n):
+        add=freq[ord(s[i])-ord('a')]%26
+        if (ord(s[i])+add<=ord('z')):
+            s[i]=chr(ord(s[i])+add)
+        else:
+            add=(ord(s[i])+add)-(ord('z'))
+            s[i]=chr(ord('a')+add-1)
+    print("".join(s))
+
+#main
+string=input("Enter a string: ")
+frequency([i for i in string])
