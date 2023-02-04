4ad63bb98ca117d3191da38b6b54d75e07598cd6
JOHN <113880870+JOHN10102003@users.noreply.github.com>
2023-02-04 14:31:00 +0530

    Update Maximum Area of Water.py

--- DAY-1/Maximum Area of Water.py
+++ DAY-1/Maximum Area of Water.py
@@ -1,9 +1,1 @@
-def maxArea(A, Len) : 
-     area = 0 
-     for i in range(Len) : 
-         for j in range(i + 1, Len) : 
-             area = max(area, min(A[j], A[i]) * (j - i)) 
-     return area 
- a = [ 1, 5, 4, 3 ] 
- len1 = len(a) 
- print(maxArea(a, len1))
+
