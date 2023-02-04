7b0669e13e43b788ffed44e16696079fd43bec59
JOHN <113880870+JOHN10102003@users.noreply.github.com>
2023-02-04 14:42:26 +0530

    Update Party in a Cruise.py

--- DAY-1/Party in a Cruise.py
+++ DAY-1/Party in a Cruise.py
@@ -1,8 +1,19 @@
-tot=int(input("enter the value of t:")) 
- e=[7,0,8,4,2] 
- l=[1,2,3,1,2] 
- x=[0,0,0,0,0] 
- for i in range(0,tot): 
-     x[i]=x[i-1]+e[i]-l[i] 
-     print(x[i]) 
- print("max guest=",max(x))
+E=[]
+L=[]
+T = int(input("Enter value of T: "))
+for i in range(T):
+    e=int(input("Enter the ENTRY list: "))
+    E.append(e)
+print(E)
+print("\n")
+for i in range(T):
+    l=int(input("Enter the LEAVING list: "))
+    L.append(l)
+print(L)
+print("\n")
+Sum=0
+Max=0
+for i in range(T):
+    Sum+=E[i]-L[i]
+    Max=max(Sum,Max)
+print("Maximum no. of guests present on cruise: ", Max)
