cb33e440ef718f21928dcfe017121358240ed19d
JOHN <113880870+JOHN10102003@users.noreply.github.com>
2023-02-04 14:31:29 +0530

    Update Maximum Area of Water.py

--- DAY-1/Maximum Area of Water.py
+++ DAY-1/Maximum Area of Water.py
@@ -1,1 +1,17 @@
 
+def maxarea(a, len):
+    area=0
+    for i in range(len):
+        for j in range(i+1, len):
+            area=max(area, min(a[j], a[i]) * (j-i))
+    return area
+
+l=[]
+a=int(input("Enter number of elements: "))
+for i in range(0,a):
+    ele=int(input("Enter the value: "))
+    l.append(ele)
+print(l)
+print("\n")
+length=len(l)
+print(maxarea(l, length))
